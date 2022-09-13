8079e0da9557c5eebc1914479ce6f8e7d1fb63a4
Dmitry S <dmitry.s@hsdn.org>
2022-09-13 13:29:11 +0400

    Add multi-servers support

--- bin/proxy.js
+++ bin/proxy.js
@@ -56,46 +56,53 @@ class TeraProxy {
     }
 
     run() {
-        // Use config for metadata
-        const metadata = {
-            dataFolder: this.dataFolder,
-            publisher: this.config.publisher,
-            language: this.config.language,
-            platform: 'pc',
-            environment: 'live',
-            majorPatchVersion: Number(this.config.patchVersion.split(".")[0]),
-            minorPatchVersion: Number(this.config.patchVersion.split(".")[1]),
-            protocolVersion: this.config.protocolVersion,
-            maps: { protocol: {}, sysmsg: {} }
-        };
+        this.config.servers.forEach(data => {
+            // Use config for metadata
+            const metadata = {
+                dataFolder: this.dataFolder,
+                serverList: this.config.servers,
+                serverId: data.serverId,
+                publisher: data.publisher,
+                language: data.language,
+                platform: 'pc',
+                environment: 'live',
+                majorPatchVersion: Number(data.patchVersion.split(".")[0]),
+                minorPatchVersion: Number(data.patchVersion.split(".")[1]),
+                protocolVersion: data.protocolVersion,
+                maps: { protocol: {}, sysmsg: {} }
+            };
 
-        // Load protocol map
-        metadata.maps.protocol = {};
-        try {
-            metadata.maps.protocol = Object.assign(LoadProtocolMap(this.dataFolder, metadata.protocolVersion), metadata.protocol);
+            // Load protocol map
+            metadata.maps.protocol = {};
+            try {
+                metadata.maps.protocol = Object.assign(LoadProtocolMap(this.dataFolder, metadata.protocolVersion), metadata.protocol);
 
-            if (Object.keys(metadata.maps.protocol).length === 0) {
-                console.warn(mui.get('proxy/warning-unmapped-protocol-1', { protocolVersion: metadata.protocolVersion, publisher: metadata.publisher, majorPatchVersion: metadata.majorPatchVersion, minorPatchVersion: metadata.minorPatchVersion }));
-                console.warn(mui.get('proxy/warning-unmapped-protocol-2'));
-                console.warn(mui.get('proxy/warning-unmapped-protocol-3'));
-                console.warn(mui.get('proxy/warning-unmapped-protocol-4'));
-                console.warn(mui.get('proxy/warning-unmapped-protocol-5'));
-                console.warn(mui.get('proxy/warning-unmapped-protocol-6'));
-                console.warn(mui.get('proxy/warning-unmapped-protocol-7'));
-                console.warn(mui.get('proxy/warning-unmapped-protocol-8', { supportUrl: global.TeraProxy.SupportUrl }));
-            } else {
-                console.log(mui.get('proxy/protocol-loaded', { protocolVersion: metadata.protocolVersion, publisher: metadata.publisher, majorPatchVersion: metadata.majorPatchVersion, minorPatchVersion: metadata.minorPatchVersion }));
+                if (Object.keys(metadata.maps.protocol).length === 0) {
+                    console.warn(mui.get('proxy/warning-unmapped-protocol-1', { protocolVersion: metadata.protocolVersion, publisher: metadata.publisher, majorPatchVersion: metadata.majorPatchVersion, minorPatchVersion: metadata.minorPatchVersion }));
+                    console.warn(mui.get('proxy/warning-unmapped-protocol-2'));
+                    console.warn(mui.get('proxy/warning-unmapped-protocol-3'));
+                    console.warn(mui.get('proxy/warning-unmapped-protocol-4'));
+                    console.warn(mui.get('proxy/warning-unmapped-protocol-5'));
+                    console.warn(mui.get('proxy/warning-unmapped-protocol-6'));
+                    console.warn(mui.get('proxy/warning-unmapped-protocol-7'));
+                    console.warn(mui.get('proxy/warning-unmapped-protocol-8', { supportUrl: global.TeraProxy.SupportUrl }));
+                } else {
+                    console.log(mui.get('proxy/protocol-loaded', { protocolVersion: metadata.protocolVersion, publisher: metadata.publisher, majorPatchVersion: metadata.majorPatchVersion, minorPatchVersion: metadata.minorPatchVersion }));
+                }
+            } catch (e) {
+                console.error(mui.get('proxy/error-cannot-load-protocol', { protocolVersion: metadata.protocolVersion, publisher: metadata.publisher, majorPatchVersion: metadata.majorPatchVersion, minorPatchVersion: metadata.minorPatchVersion }));
+                console.error(e);
             }
-        } catch (e) {
-            console.error(mui.get('proxy/error-cannot-load-protocol', { protocolVersion: metadata.protocolVersion, publisher: metadata.publisher, majorPatchVersion: metadata.majorPatchVersion, minorPatchVersion: metadata.minorPatchVersion }));
-            console.error(e);
-        }
 
-        // Create a new server
-        const net = require('net');
-        const server = net.createServer(socket => this.connectionManager.start({ ip: this.config.serverIp, port: this.config.serverPort }, socket, metadata));
+            // Create a new server
+            const net = require('net');
+            const server = net.createServer(socket => this.connectionManager.start({ ip: data.serverIp, port: data.serverPort }, socket, metadata));
 
-        server.listen(this.config.listenPort, this.config.listenIp);
+            server.listen(data.listenPort, data.listenIp, () => {
+                const { address: listen_ip, port: listen_port } = server.address();
+                console.log(mui.get('proxy/redirecting-server', { name: data.name, publisher: metadata.publisher, serverId: metadata.serverId, listen_ip, listen_port, ip: data.serverIp, port: data.serverPort }));
+            });
+        });
     }
 }
 
